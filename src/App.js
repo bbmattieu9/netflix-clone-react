86ba7553bd68727a50b9b742bf0e523707eb4c3d
Babatoonday Mattieu <32313099+bbmattieu9@users.noreply.github.com>
2020-12-06 22:47:54 +0100

    [App] add REQUEST object for api endpoints as an exported file with API KEY

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 
 function App() {
+
+  // 49ff79ae71652e665b4084b23dd0a138 tmdb api key
   return (
     <div className="App">
-      
+      <h1>Lets build netflix clone</h1>
     </div>
   );
 }
