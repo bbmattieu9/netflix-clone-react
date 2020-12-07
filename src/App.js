2c5c0e0e55214b773974732bac05ddcc87387d69
Babatoonday Mattieu <32313099+bbmattieu9@users.noreply.github.com>
2020-12-07 22:33:28 +0100

    [Row] create movie rowsc requests module and axios instance for api calls

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-import './App.css';
+import Row from './Row';
+import requests from './request';
 
 function App() {
 
   // 49ff79ae71652e665b4084b23dd0a138 tmdb api key
   return (
     <div className="App">
-      <h1>Lets build netflix clone</h1>
+      <h1>Hello Netflix Clone!</h1>
+      <Row title='Netflix Originals' fetchUrl={requests.fetchNetflixOriginals} />
+      <Row title='Trending Movies'  fetchUrl={requests.fetchTrending} />
     </div>
   );
 }
