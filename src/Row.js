fd803f4d412c7ad56ba4194e504af3771307622a
Babatoonday Mattieu <32313099+bbmattieu9@users.noreply.github.com>
2020-12-09 12:35:27 +0100

    [Row] add image full path to returned set of image url

--- src/Row.js
+++ src/Row.js
@@ -1,25 +1,38 @@
 import axios from './axios';
-import React, {  useEffect } from 'react'
+import React, {  useEffect, useState } from 'react';
+
+
+const base_url = 'https://image.tmdb.org/t/p/original/';
 
 function Row({ title, fetchUrl }) {
 
-    // const [movies, setMovies] = useState([]);
+    const [movies, setMovies] = useState([]);
 
 
 
     useEffect(() => {
         async function fetchData() {
             const request = await axios.get(fetchUrl);
-            console.log('[Requests from Row Component]:', request);
+            console.log('[movies]: ', request.data.results);
+            setMovies(request.data.results);
             return request;
         }
 
         fetchData();
-    },);
+    },[fetchUrl]);
 
     return (
-        <div>
+        <div className='row'>
             <h2>{title}</h2>
+
+
+            <div className="row__posters">
+                {movies.map(movie => (
+                    <img
+                        className="row__poster"
+                        src={`${base_url}${movie.poster_path}`} alt={movie.name} key={movie.id}/>
+                ))}
+            </div>
         </div>
     )
 }
