917f07862c6b074df5eee8355be0159091011c25
Babatoonday Mattieu <32313099+bbmattieu9@users.noreply.github.com>
2020-12-13 00:33:46 +0100

    [Nav] add Nav content

--- src/Nav.js
+++ src/Nav.js
@@ -1,8 +1,36 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import './Nav.css';
+
 
 function Nav() {
+
+    const [show, handleShow] = useState(false);
+
+    useEffect(() => {
+        window.addEventListener('scroll', () => {
+            if (window.scrollY > 100) {
+                handleShow(true);
+            } else handleShow(false);
+        });
+        return () => {
+            window.removeEventListener('scroll');
+        }
+    }, [])
+
+    const netflixLogo ='https://logos-download.com/wp-content/uploads/2016/03/Netflix_logo-700x189.png';
+    const netflixAvatar = 'https://pbs.twimg.com/media/DlKNEufWsAAgr2E.jpg';
     return (
-        <div>
+        <div className={`nav ${show && "nav__black"}`}>
+            <img
+                className='nav__logo'
+                src={netflixLogo}
+                alt='Netflix Logo'
+            />
+
+            <img  
+                className='nav__avatar'
+                src={netflixAvatar}
+                alt='Netflix Avatar' />
             
         </div>
     )
